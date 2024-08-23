90a7da973e009c6f58b08e2d21596827b83c3ef2
joseph-sentry <joseph.sawaya@sentry.io>
2024-08-23 17:06:08 -0400

    remove unused parsers

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@ use pyo3::prelude::*;
 
 mod failure_message;
 mod junit;
-mod pytest_reportlog;
 mod testrun;
-mod vitest_json;
 
 pyo3::create_exception!(test_results_parser, ParserError, PyException);
 
@@ -17,11 +15,6 @@ fn test_results_parser(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_class::<testrun::Outcome>()?;
 
     m.add_function(wrap_pyfunction!(junit::parse_junit_xml, m)?)?;
-    m.add_function(wrap_pyfunction!(
-        pytest_reportlog::parse_pytest_reportlog,
-        m
-    )?)?;
-    m.add_function(wrap_pyfunction!(vitest_json::parse_vitest_json, m)?)?;
     m.add_function(wrap_pyfunction!(failure_message::build_message, m)?)?;
     m.add_function(wrap_pyfunction!(
         failure_message::escape_failure_message,
