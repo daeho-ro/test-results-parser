93a3b78d2f33472dfc6025fc201318afaba49256
joseph-sentry <joseph.sawaya@sentry.io>
2024-10-18 11:45:53 -0400

    test: actually test the empty file

--- tests/test_junit.py
+++ tests/test_junit.py
@@ -262,6 +262,13 @@ tests/test_parsers.py:16: AssertionError""",
                     ],
                 ),
             ),
+            (
+                "./tests/testsuites.xml",
+                ParsingInfo(
+                    None,
+                    [],
+                ),
+            ),
         ],
     )
     def test_junit(self, filename, expected):
